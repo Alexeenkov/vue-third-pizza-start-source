323de95ac04a0bd43f662c59830a0c5071030003
alekseenkov.ilya <alexeenkov-frontend@yandex.ru>
2024-10-28 09:22:57 +0300

    Вёрстка главной страницы и хэдера

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <AppHeader />
+
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import AppHeader from "@/layouts/AppHeader.vue";
+</script>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,15 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/_global.scss";
+        `,
+      },
+    },
+  },
   test: {
     environment: "jsdom",
   },
